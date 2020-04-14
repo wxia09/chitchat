5d243ea1cf3b0f1bce95d2f844d5a100f0b3abef
wxia <qlhdtly12189@163.com>
2020-04-14 22:39:19 +0800

    盒子单边可以拖动, 但是有bug, 也需要封装

--- src/components/DragSize/index.jsx
+++ src/components/DragSize/index.jsx
@@ -3,14 +3,25 @@ import PropsType from "prop-types";
 import "./scss/index.scss";
 
 class DragSize extends React.Component {
-  state = {
-    widowsWidth: 0,
-    widowsHeight: 0,
-    boxWidth: 0,
-    boxHeight: 0,
-    left: 0,
-    top: 0,
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      widowsWidth: 0,
+      widowsHeight: 0,
+      boxWidth: 0,
+      boxHeight: 0,
+      left: 0,
+      top: 0,
+    };
+    this.topLeft = this.topLeft.bind(this);
+    this.topRight = this.topRight.bind(this);
+    this.top = this.top.bind(this);
+    this.bottomLeft = this.bottomLeft.bind(this);
+    this.bottomRight = this.bottomRight.bind(this);
+    this.right = this.right.bind(this);
+    this.left = this.left.bind(this);
+    this.bottom = this.bottom.bind(this);
+  }
   static propsType = {
     top: PropsType.bool,
     right: PropsType.bool,
@@ -47,6 +58,34 @@ class DragSize extends React.Component {
       top,
     });
   }
+  topLeft(e) {
+    // console.log(e.target);
+    let ele = e.target;
+    console.log(e.pageX, e.pageY);
+    let { boxWidth, boxHeight, left, top, widowsWidth, widowsHeight } = this.state;
+    let { pageX, pageY } = e;
+    ele.onmousemove = (el) => {
+      let nowX = el.pageX;
+      let nowY = el.pageY;
+      this.setState({
+        boxWidth: Math.min(boxWidth - (nowX - pageX), widowsWidth),
+        boxHeight: Math.min(boxHeight - (nowY - pageY), widowsHeight),
+        left: Math.max(left + (nowX - pageX), 0),
+        top: Math.max(top + (nowY - pageY), 0),
+      });
+    };
+    document.onmouseup = () => {
+      ele.onmousemove = null;
+      document.onmouseup = null;
+    };
+  }
+  topRight() {}
+  top() {}
+  bottomLeft() {}
+  bottomRight() {}
+  right() {}
+  left() {}
+  bottom() {}
   render() {
     let { boxWidth, boxHeight, left, top } = this.state;
     let boxStyle = {
@@ -58,7 +97,7 @@ class DragSize extends React.Component {
     };
     return (
       <div className="flex drag-size" style={boxStyle}>
-        <div className="top left corner" style={{ cursor: "nw-resize" }} />
+        <div className="top left corner" style={{ cursor: "nw-resize" }} onMouseDown={this.topLeft} />
         <div className="top right corner" style={{ cursor: "ne-resize" }} />
         <div className="bottom left corner" style={{ cursor: "sw-resize" }} />
         <div className="bottom right corner" style={{ cursor: "se-resize" }} />
