38062bc4f99d9d1ada635a5b550a981f6984a276
wxia <qlhdtly12189@163.com>
2020-04-16 22:47:53 +0800

    items

---
+++ src/components/DragSize/data/data.js
@@ -0,0 +1,42 @@
+export const items = [
+  {
+    type: "top.left",
+    cursor: "nw-resize",
+    cssStyle: "top left corner",
+  },
+  {
+    type: "top.right",
+    cursor: "ne-resize",
+    cssStyle: "top right corner",
+  },
+  {
+    type: "bottom.left",
+    cursor: "sw-resize",
+    cssStyle: "bottom left corner",
+  },
+  {
+    type: "bottom.right",
+    cursor: "se-resize",
+    cssStyle: "bottom right corner",
+  },
+  {
+    type: "bottom",
+    cursor: "s-resize",
+    cssStyle: "bottom middle",
+  },
+  {
+    type: "top",
+    cursor: "n-resize",
+    cssStyle: "top middle",
+  },
+  {
+    type: "left",
+    cursor: "w-resize",
+    cssStyle: "left middle",
+  },
+  {
+    type: "right",
+    cursor: "e-resize",
+    cssStyle: "right middle",
+  },
+];
