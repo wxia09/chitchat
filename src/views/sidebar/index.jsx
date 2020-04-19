2b29d52cf509440414b49cac5d7f120b258ab5c9
wxia <qlhdtly12189@163.com>
2020-04-19 22:46:49 +0800

    侧边栏修改 doing

--- src/views/sidebar/index.jsx
+++ src/views/sidebar/index.jsx
@@ -1,8 +1,16 @@
 import React, { Component } from "react";
+import "./scss/index.scss";
 
 class Sidebar extends Component {
   render() {
-    return <div style={{ width: "100px" }}>我是侧边栏</div>;
+    return (
+      <div className="sidebar">
+        <div className="avatar">
+          <span />
+          <img src={process.env.PUBLIC_URL + "/images/defaultAvatar.jpg"} alt="" width="40" height="40" />
+        </div>
+      </div>
+    );
   }
 }
 
