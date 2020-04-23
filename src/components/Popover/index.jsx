18cb65c8ff050e38c416fa27678870072c70748d
wxia <qlhdtly12189@163.com>
2020-04-23 22:25:50 +0800

    popover弹出层简单版完成

--- src/components/Popover/index.jsx
+++ src/components/Popover/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./scss/index.scss";
+import PropsType from "prop-types";
 
 class Popover extends React.Component {
   constructor(props) {
@@ -9,6 +10,20 @@ class Popover extends React.Component {
     };
     this.handleShow = this.handleShow.bind(this);
   }
+
+  static propsType = {
+    wrap: PropsType.string,
+    placement: PropsType.string,
+    content: PropsType.string,
+    title: PropsType.string,
+  };
+
+  static defaultProps = {
+    wrap: "",
+    placement: "bottom",
+    content: "",
+    title: "",
+  };
 
   handleShow() {
     const self = this;
@@ -34,7 +49,6 @@ class Popover extends React.Component {
     if (!props.children) {
       throw new Error("必须传子组件");
     }
-    console.log(props.children);
     let children = props.children instanceof Array ? [...props.children] : [props.children];
     let firstChild = children.splice(0, 1)[0];
     return (
@@ -44,11 +58,10 @@ class Popover extends React.Component {
         })}
         {children.length ? (
           React.cloneElement(children[0], {
-            className:
-              (this.state.show ? "show " : "hide ") + children[0].props.className + " " + children[0].props.wrap || "",
+            className: (this.state.show ? "show " : "hide ") + children[0].props.className + " " + props.wrap,
           })
         ) : (
-          <div className={(this.state.show ? "show " : "hide ") + "wrap"}>
+          <div className={(this.state.show ? "show " : "hide ") + "wrap " + props.placement}>
             <div className={"title border-bottom"}>{props.title}</div>
             <div className={"content"}>{props.content}</div>
           </div>
--- src/views/sidebar/index.jsx
+++ src/views/sidebar/index.jsx
@@ -6,13 +6,11 @@ class Sidebar extends Component {
   render() {
     return (
       <div className="sidebar">
+        <div style={{ height: "25px" }} />
         <Popover content={"我是内容啊"} title={"我是标题啊"}>
           <div className="avatar">
             <span />
             <img src={process.env.PUBLIC_URL + "/images/defaultAvatar.jpg"} alt="" width="40" height="40" />
-          </div>
-          <div className={"ffs"} wrap={""}>
-            8883344
           </div>
         </Popover>
       </div>
