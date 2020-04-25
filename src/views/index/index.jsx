550fb0a2fb69f87524718ad6d5e1f96f15664a3c
wxia <qlhdtly12189@163.com>
2020-04-25 22:22:59 +0800

    引入iconfont

--- src/views/index/index.jsx
+++ src/views/index/index.jsx
@@ -3,6 +3,7 @@ import Sidebar from "../sidebar";
 import Friends from "../friends";
 import News from "../news";
 import DragSize from "../../components/DragSize";
+import "../../static/font/iconfont.css";
 
 class Index extends Component {
   render() {
