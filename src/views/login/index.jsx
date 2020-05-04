9a9997ff9e8bb50c7014bc10d0f3138b2850aec8
wixa <qlhdtly12189@163.com>
2020-05-04 19:59:41 +0800

    登录功能正常, 但是没有input框的验证

--- src/views/login/index.jsx
+++ src/views/login/index.jsx
@@ -1,44 +1,64 @@
 import React from "react";
 
 import "./scss/index.scss";
-import axios from "axios";
+import { http } from "../../utils/http";
 
 class Login extends React.Component {
-  state = {
-    username: "12",
-    password: "111111",
-  };
+  constructor(props) {
+    super(props);
+    this.handlerUsername = this.handlerUsername.bind(this);
+    this.handlerPassword = this.handlerPassword.bind(this);
+    this.handleReset = this.handleReset.bind(this);
+    this.handleLogin = this.handleLogin.bind(this);
+    this.state = {
+      username: "",
+      password: "",
+    };
+  }
   handleLogin() {
     let { username, password } = this.state;
-    axios
-      .post("http://localhost:3005/login", { username, password })
+    let { history } = this.props;
+    http
+      .post("login", { username, password })
       .then((res) => {
-        console.log(res.data.data);
-        return axios.post("http://localhost:3005/auth/user", null, {
-          headers: {
-            authorization: res.data.token,
-          },
-        });
-      })
-      .then((res) => {
-        console.log(res);
+        let { data, token } = res.data;
+        if (token) {
+          window.localStorage.setItem("token", token);
+          history.replace("/");
+        }
       })
       .catch((err) => console.log(err));
+  }
+  handlerPassword(e) {
+    this.setState({
+      password: e.target.value,
+    });
+  }
+  handlerUsername(e) {
+    this.setState({
+      username: e.target.value,
+    });
+  }
+  handleReset() {
+    this.setState({
+      username: "",
+      password: "",
+    });
   }
   render() {
     return (
       <div className="login">
         <div>
           <label htmlFor="user">账号</label>
-          <input type="text" id="user" />
+          <input type="text" id="user" onChange={this.handlerUsername} />
         </div>
         <div>
           <label htmlFor="password">密码:</label>
-          <input type="password" id="password" />
+          <input type="password" id="password" onChange={this.handlerPassword} />
         </div>
         <div className="button">
-          <button>重置</button>
-          <button onClick={this.handleLogin.bind(this)}>确定</button>
+          <button onClick={this.handleReset}>重置</button>
+          <button onClick={this.handleLogin}>确定</button>
         </div>
       </div>
     );
