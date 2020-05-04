a45780cba2fc39186f31e5efd82dbc427f57f962
wixa <qlhdtly12189@163.com>
2020-05-04 19:57:17 +0800

    使用Router的history, 可以用history包进行js页面跳转

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,20 +1,29 @@
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { Router, Route, Switch } from "react-router-dom";
 import React from "react";
 import Home from "../views/index/index";
 import Register from "../views/register";
 import Login from "../views/login";
+import history from "../assets/history";
 
-export default class Router extends React.Component {
+/*
+ * Router 是所有路由组件共用的底层接口。通常，我们的应用程序将使用其中一个高级路由器代替：
+ *   <BrowserRouter>
+ *   <HashRouter>
+ *   <MemoryRouter>
+ *   <NativeRouter>
+ *   <StaticRouter>
+ * */
+export default class RouterIndex extends React.Component {
   render() {
     return (
-      <BrowserRouter>
+      <Router history={history}>
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/home" component={Home} />
           <Route exact path="/res" component={Register} />
           <Route exact path="/login" component={Login} />
         </Switch>
-      </BrowserRouter>
+      </Router>
     );
   }
 }
