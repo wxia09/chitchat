c245bdcdd597a0b087157f1508caa59976f5e338
wxia <qlhdtly12189@163.com>
2020-05-08 22:42:14 +0800

    添加好友的简单布局

--- src/views/friends/index.jsx
+++ src/views/friends/index.jsx
@@ -1,22 +1,39 @@
 import React, { Component } from "react";
 import "./scss/index.scss";
 import { FriendList } from "./component/item";
-import Popover from "../../components/Popover";
+import Dialog from "../../components/Dialog";
 
 class Friends extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showAddFriend: false,
+    };
+    this.handleShowAddFriend = this.handleShowAddFriend.bind(this);
+  }
+  handleShowAddFriend() {
+    this.setState({
+      showAddFriend: !this.state.showAddFriend,
+    });
+  }
   render() {
     return (
-      <div className={"friend"}>
-        <div className={"search"}>
-          <span className="iconfont icon-search" />
-          <input type="text" />
-          <Popover>
-            <span className="iconfont icon-add" />
-            <div style={{ position: "absolute" }}>我是添加好友</div>
-          </Popover>
+      <>
+        <div className={"friend"}>
+          <div className={"search"}>
+            <span className="iconfont icon-search" />
+            <input type="text" />
+            <span className="iconfont icon-add" onClick={this.handleShowAddFriend} />
+          </div>
+          <FriendList />
         </div>
-        <FriendList />
-      </div>
+        <Dialog show={this.state.showAddFriend} title="添加好友" width="800px" height="600px">
+          <section className="search-friends">
+            <input type="text" placeholder="请输入添加好的账号/昵称/姓名" className="search-input" />
+            <button className="search-btn">检索</button>
+          </section>
+        </Dialog>
+      </>
     );
   }
 }
