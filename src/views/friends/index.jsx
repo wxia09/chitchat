f63fc35e593a87f333cfffafa35ce631df7a7496
wxia <qlhdtly12189@163.com>
2020-05-10 22:52:57 +0800

    添加好友检索后的列表

--- src/views/friends/index.jsx
+++ src/views/friends/index.jsx
@@ -2,18 +2,37 @@ import React, { Component } from "react";
 import "./scss/index.scss";
 import { FriendList } from "./component/item";
 import Dialog from "../../components/Dialog";
+import Popover from "../../components/Popover";
+
+import { http } from "../../utils/http";
 
 class Friends extends Component {
   constructor(props) {
     super(props);
     this.state = {
       showAddFriend: false,
+      inputValue: "12",
+      extendList: [],
     };
     this.handleShowAddFriend = this.handleShowAddFriend.bind(this);
+    this.handlerValue = this.handlerValue.bind(this);
+    this.handleSearch = this.handleSearch.bind(this);
   }
   handleShowAddFriend() {
     this.setState({
       showAddFriend: !this.state.showAddFriend,
+    });
+  }
+  handlerValue(e) {
+    this.setState({
+      inputValue: e.target.value,
+    });
+  }
+  handleSearch() {
+    http("/friend/extend", { params: { keywords: this.state.inputValue } }).then((res) => {
+      this.setState({
+        extendList: res.data.data,
+      });
     });
   }
   render() {
@@ -35,8 +54,27 @@ class Friends extends Component {
           cancel={this.handleShowAddFriend}
         >
           <section className="search-friends">
-            <input type="text" placeholder="请输入添加好的账号/昵称/姓名" className="search-input" />
-            <button className="search-btn">检索</button>
+            <input
+              type="text"
+              placeholder="请输入添加好的账号/昵称/姓名"
+              className="search-input"
+              value={this.state.inputValue}
+              onChange={this.handlerValue}
+            />
+            <button className="search-btn" onClick={this.handleSearch}>
+              检索
+            </button>
+          </section>
+          <section className={"extend-list"}>
+            {this.state.extendList.map((item) => (
+              <FriendList item={item} key={item.id}>
+                <div className="flex-sub" />
+                <Popover trigger="hover">
+                  <i className="iconfont icon-add cursor-pointer" />
+                  <div className="friend-add-btn">添加</div>
+                </Popover>
+              </FriendList>
+            ))}
           </section>
         </Dialog>
       </>
