8201f5ee6446f5bb6a7e41655f91c978588465ae
wxia <qlhdtly12189@163.com>
2020-05-20 22:29:14 +0800

    侧边栏布局

--- src/views/sidebar/index.jsx
+++ src/views/sidebar/index.jsx
@@ -4,8 +4,46 @@ import "./scss/index.scss";
 import Popover from "../../components/Popover";
 
 class Sidebar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      iconList: [
+        {
+          icon: "friends02",
+          tip: "信息",
+          active: true,
+        },
+        {
+          icon: "friend1",
+          tip: "通讯录",
+          active: false,
+        },
+      ],
+      oldIconIndex: 0,
+    };
+    this.handleActive = this.handleActive.bind(this);
+  }
+
+  handleActive(index) {
+    let { iconList, oldIconIndex } = this.state;
+    if (oldIconIndex === index) return;
+    let array = [...iconList];
+    let item = { ...array[index] };
+    item.active = !item.active;
+    if (oldIconIndex > -1) {
+      let oldItem = { ...array[oldIconIndex] };
+      oldItem.active = false;
+      array.splice(oldIconIndex, 1, oldItem);
+    }
+    array.splice(index, 1, item);
+    this.setState({
+      iconList: array,
+      oldIconIndex: index,
+    });
+  }
   render() {
     let { userInfo } = this.props;
+    let { iconList } = this.state;
     return (
       <div className="sidebar">
         <div style={{ height: "25px" }} />
@@ -15,6 +53,17 @@ class Sidebar extends Component {
             <img src={process.env.PUBLIC_URL + "/images/defaultAvatar.jpg"} alt="" width="40" height="40" />
           </div>
         </Popover>
+        {iconList.map((item, index) => (
+          <div
+            className="slider-icon text-center"
+            onClick={() => {
+              this.handleActive(index);
+            }}
+            key={index}
+          >
+            <i className={"iconfont icon-" + item.icon + (item.active ? " active" : "")} />
+          </div>
+        ))}
       </div>
     );
   }
