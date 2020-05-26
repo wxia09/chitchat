495f80f4df2d4bde084374e6a2f2220365aa5b2c
wxia <qlhdtly12189@163.com>
2020-05-26 23:17:35 +0800

    reducers引入chat模块

---
+++ src/reducers/chat/index.js
@@ -0,0 +1,19 @@
+import { CURRENT_SLIDER_STATUS } from "./types";
+let initState = {
+  currentSliderStatus: 0,
+};
+
+function reducer(state = initState, action) {
+  let obj = { ...state };
+  const actionFn = {
+    [CURRENT_SLIDER_STATUS]() {
+      return {
+        ...obj,
+        [CURRENT_SLIDER_STATUS]: action.value,
+      };
+    },
+  };
+  return (actionFn[action.type] && actionFn[action.type]()) || obj;
+}
+
+export default reducer;
