13d1187b379d0b9876191b18c29da1e3e3920201
wxia <qlhdtly12189@163.com>
2020-05-31 23:47:37 +0800

    除去log

--- src/reducers/friends/thunk.js
+++ src/reducers/friends/thunk.js
@@ -2,9 +2,7 @@ import { http } from "../../utils/http";
 import { FRIENDS_APPLY_LIST } from "./types";
 
 export const setFriendsApplyList = (() => {
-  console.log(222);
   return (dispatch) => {
-    console.log(999);
     http("friend/apply/list")
       .then((res) => {
         dispatch({ type: FRIENDS_APPLY_LIST, value: res.data.data });
