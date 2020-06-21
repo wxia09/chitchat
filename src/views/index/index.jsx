24a3af6ebcdd55052156c3f7ed5d9cc3262e6135
wxia <qlhdtly12189@163.com>
2020-06-21 11:51:06 +0800

    socket(测试)

--- src/views/index/index.jsx
+++ src/views/index/index.jsx
@@ -6,11 +6,31 @@ import Chat from "../chat/index";
 import "../../static/font/iconfont.css";
 import { http } from "../../utils/http";
 
+import io from "socket.io-client";
+
 class Index extends Component {
   componentDidMount() {
     http("auth/user")
       .then((res) => {
+        let { data } = res.data;
         this.props.setUserInfo(res.data.data);
+        let socket = io(process.env.REACT_APP_SOCKET_URL, {
+          query: {
+            token: this.props.token,
+          },
+        });
+        socket.emit("connect");
+        socket.on("receive", (data) => {
+          console.log(data, 999999);
+        });
+        if (data.id === 2) {
+          setTimeout((_) => {
+            socket.emit("send", {
+              message: "我给1发送",
+              receiveId: 1,
+            });
+          }, 1000);
+        }
       })
       .catch((err) => {
         console.log(err);
@@ -30,6 +50,7 @@ export default connect(
   function mapStateToProps(state) {
     return {
       userInfo: state.defaultReducer.userInfo,
+      token: state.defaultReducer.token,
     };
   },
   function mapDispatchToProps(dispatch) {
