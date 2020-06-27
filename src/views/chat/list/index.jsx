070c28a97b109de2fc7f8f2bfa390cfa1b39543d
wxia <qlhdtly12189@163.com>
2020-06-27 20:58:22 +0800

    links

--- src/views/chat/list/index.jsx
+++ src/views/chat/list/index.jsx
@@ -1,10 +1,49 @@
 // 联系人列表(历史)
 import React from "react";
+import { Item } from "../components/item";
+import Roof from "../roof";
+
+import { getLinksList } from "../../../reducers/chat/thunk";
+import { connect } from "react-redux";
 
 class List extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentIndex: 0,
+    };
+    this.handlerChatWin = this.handlerChatWin.bind(this);
+  }
+  componentDidMount() {
+    this.props.dispatch(getLinksList);
+  }
+  handlerChatWin(index) {
+    this.setState({
+      currentIndex: index,
+    });
+  }
   render() {
-    return <div>list</div>;
+    return (
+      <div>
+        <Roof />
+        {this.props.linksList.map((item, index) => (
+          <Item
+            item={item}
+            key={item.id}
+            nameKey="sendName"
+            badge={true}
+            badgeKey="unread_count"
+            active={this.state.currentIndex === index}
+            onClick={this.handlerChatWin.bind(index)}
+          />
+        ))}
+      </div>
+    );
   }
 }
 
-export default List;
+export default connect(function mapStateToProps(state) {
+  return {
+    linksList: state.chat.linksList,
+  };
+})(List);
