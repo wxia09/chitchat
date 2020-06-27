1c4c6a2b823885381b1a8146cf54fcf04fc6e87e
wxia <qlhdtly12189@163.com>
2020-06-27 20:57:33 +0800

    角标, active

--- src/views/chat/components/item.jsx
+++ src/views/chat/components/item.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import "./scss/item.scss";
 export const Item = (props) => {
-  let { item = {} } = props;
+  let { item = {}, nameKey = "name", badge = false, badgeKey = "badge", active = false } = props;
   return (
-    <div className={"friend-list flex align-center hover"} onClick={props.onClick || (() => {})}>
+    <div
+      className={(active ? "active-item " : "") + "friend-list flex align-center hover"}
+      onClick={props.onClick || (() => {})}
+    >
       <div className={"avatar-name flex align-center"}>
         <div className={"avatar"}>
           <img src={process.env.PUBLIC_URL + "/images/defaultAvatar.jpg"} alt="" />
+          {badge ? <div className="badge text-center">{item[badgeKey]}</div> : []}
         </div>
-        <div className={"nickname"}>{item.name}</div>
+        <div className={"nickname"}>{item[nameKey]}</div>
       </div>
       {props.children}
     </div>
